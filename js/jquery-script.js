c02ad96ff26edb3dd4dff75e861094f1b5e59378
Daniel Seal <84367021+sealdaniel@users.noreply.github.com>
2023-02-09 04:28:20 -0300

    Add files via upload

---
+++ js/jquery-script.js
@@ -0,0 +1,16 @@
+//efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+
+  $("#botao-cadastrar").click(function(){
+
+    $("#form-cadastrar").slideToggle("slow")
+    $("#section-login").slideToggle("slow")
+    $("#botao-cadastrar").hide()
+
+  });
+
+});
+
+//rede social
+
---
+++ js/script.js
@@ -0,0 +1,39 @@
+//efeito do botão voltar ao Topo
+function topo(){
+  window.scrollTo(
+    {
+      top:0,
+      left:0,
+      behavior:'smooth'
+    }
+  )
+}
+
+//Validação de Login
+
+function login(){
+  var logado = 0;
+  var usuario = document.getElementById("usuario").value;
+  var senha = document.getElementById("senha").value;
+
+  if(usuario == 'admin' && senha == '123456'){
+    window.location = "index.html";
+    logado = 1;
+  }
+
+  if(logado == 0){
+      alert("Acesso negado. Dados incorretos!");
+  }
+}
+
+
+//Ativar alert no botão cadastrar
+
+function cadastro() {
+  alert("Cadastro realizado com sucesso!");
+  window.location.href = "index.html";
+}
+
+
+//redes sociais
+
